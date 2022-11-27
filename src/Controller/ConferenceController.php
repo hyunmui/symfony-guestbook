ef63870b4bcc493022c5f458970363b383039c85
Hyeonmin Seon <hmsun.es@esgroup.net>
2022-11-27 10:03:53 +0900

    진행중

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -18,6 +18,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 use Twig\Error\LoaderError;
@@ -89,6 +91,7 @@ class ConferenceController extends AbstractController
         Request $request,
         Conference $conference,
         CommentRepository $commentRepository,
+        NotifierInterface $notifier,
         string $photoDir
     ) {
         $comment = new Comment();
@@ -117,7 +120,19 @@ class ConferenceController extends AbstractController
             ];
             $this->bus->dispatch(new CommentMessage($comment->getId(), $context));
 
+            $notifier->send(new Notification(
+                'Thank you for the feedback; your comment will be after moderation.',
+                ['browser']
+            ));
+
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+
+        if ($form->isSubmitted()) {
+            $notifier->send(new Notification(
+                'Can you check your submission? There are some problems with it.',
+                ['browser']
+            ));
         }
 
         $offset = max(0, $request->query->getInt('offset', 0));
